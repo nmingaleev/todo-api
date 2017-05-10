8cb57de5d938acf2e0a29689a9e2b61138703eec
nmingaleev <nmingaleev@yandex.ru>
2017-05-09 22:21:40 -0700

    Add tests for GET /todos (somethin wrong with the tests)

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require('supertest');
 var {app} = require('./../server.js');
 var {Todo} = require('./../models/todo.js');
 
+const todos = [
+  {
+    text: 'First test to do'
+  },
+  {
+    text: 'Second test to do'
+  }
+];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done();
-  })
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,10 +33,10 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) {
           return done(err);
-        }
+        };
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
+          expect(todos.length).toBe(2);
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e));
@@ -50,5 +59,17 @@ describe('POST /todos', () => {
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
