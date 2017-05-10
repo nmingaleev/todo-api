4c6303a9b9504b5087d5dc2d461ddbfd5a933aea
nmingaleev <nmingaleev@yandex.ru>
2017-05-09 19:28:40 -0700

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -9,25 +9,13 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {// Creating new todo
-//   var todo = new Todo({
-//     text: req.body.text
-//   });
-//
-//   todo.save().then((doc) => {
-//     res.send(doc);
-//   }, (e) => {
-//     res.status(400).send(e);
-//   })
-// });
-
 app.post('/todos', (req, res) => {
-  var newTodo = new Todo({
+  var todo = new Todo({
     text: req.body.text,
     completed: req.body.completed
   })
 
-  newTodo.save().then((doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);
@@ -38,6 +26,7 @@ app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
+module.exports = {app}
 
 // var newTodo = new Todo({
 //   text: 'Walk the dog',
